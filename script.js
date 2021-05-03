c090bb3e492161c7923f8f2b39efbba7ac4abdc3
meomaymap <79776624+meomaymap@users.noreply.github.com>
2021-05-03 13:43:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ function displayWindowSize() {
 	var element = ECN("iFrame_content",0)	// = h - EID("Header").offsetHeight + "px";
 	if (typeof(element) != 'undefined' && element != null)
 	{
-	  alert(12);
+	  ECN("overlay",0).style.top = ECN("Header",0).offsetHeight + "px";
 	}
 	
 	for(var i=0; i<DisplaySize[0].length;i++){
