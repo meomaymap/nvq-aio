889603d8925d43981348f276cce71f8ee89901bb
meomaymap <79776624+meomaymap@users.noreply.github.com>
2021-05-04 17:22:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-if (typeof(Storage) !== "undefined") {
-  // Store
-  localStorage.setItem("lastname", "Smith");
-  // Retrieve
-  document.getElementById("Quangtest").innerHTML = localStorage.getItem("lastname");
-} else {
-  document.getElementById("Quangtest").innerHTML = "Sorry, your browser does not support Web Storage...";
-}
-
 function preventPullToRefresh(element) {
 	var prevent = false;
 
